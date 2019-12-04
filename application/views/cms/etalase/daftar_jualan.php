47d01102c4fec5ceb71ed0ca778d2d2f228d3dae
aquarink <junkuwieazy@gmail.com>
2019-12-04 10:46:59 +0700

    penambahan tabel-tabel pada page yang mandatory tabel

--- application/views/cms/etalase/daftar_jualan.php
+++ application/views/cms/etalase/daftar_jualan.php
@@ -1,14 +1,113 @@
 <div class="row">
 	<div class="col-md-12 col-sm-12 col-xs-12">
 		<div class="x_panel tile alert alert-success">
-			<div class="x_title">
-				<h2>Analisa Konten</h2>
-				<div class="clearfix"></div>
-			</div>
 			<div class="x_content">
-				<h4>Banyaknya konten dilihat</h4>
+				<div class="x_panel">
+              		<div class="x_title">
+	                    <ul class="nav navbar-right panel_toolbox">
+                      		<li class="dropdown">
+                        		<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-print"></i> Export</a>
+                    			<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+	                            	<a class="dropdown-item" href="#">Spreadsheet</a>
+	                            	<a class="dropdown-item" href="#">PDF</a>
+                          		</div>
+                      		</li>
+                		</ul>
+                		<div class="clearfix"></div>
+              		</div>
 
-			</div>
-		</div>
+              		<div class="x_content">
+
+	                	<div class="table-responsive">
+	                  		<table class="table table-striped jambo_table bulk_action">
+	                    		<thead>
+	                      			<tr class="headings">
+			                            <th class="column-title">Short URL</th>
+			                            <th class="column-title">Nama Barang</th>
+			                            <th class="column-title">Kategori</th>
+			                            <th class="column-title">Sub Kategori</th>
+			                            <th class="column-title">Status </th>
+			                            <th class="column-title no-link last">
+			                            	<span class="nobr">Action</span>
+			                            </th>
+	                      			</tr>
+	                    		</thead>
+
+	                    		<tbody>
+	                      			<tr class="even pointer">
+			                            <td class=" "><a href="http://waktu.my.id/short/dkfdf8">dkfdf8</a></td>
+			                            <td class=" ">Kisah Hidup Nabi Ayun AS</td>
+			                            <td class=" ">Kisah</td>
+			                            <td class=" ">Kisah Nabi</td>
+			                            <td class=" ">
+			                            	<span class="badge badge-warning">Menunggu Moderasi</span>
+			                            	<span class="badge badge-danger">Ditolak</span>
+			                            	<span class="badge badge-success">Tayang</span>
+			                            </td>
+			                            <td class=" ">
+			                            	<ul class="nav nav-pills" role="tablist">
+                    							<li role="presentation" class="dropdown">
+                      								<a href="#" class="dropdown-toggle" data-toggle="dropdown" style="padding: 0px">Action<span class="caret"></span></a>
+                              						<div class="dropdown-menu">
+                                  						<a class="dropdown-item" href="#">Edit</a>
+                                  						<a class="dropdown-item" href="#">Lihat</a>
+                                  						<a class="dropdown-item" data-toggle="modal" data-target=".modalaLihat1">Hapus</a>
+                                  					</div>
+                    							</li>
+                  							</ul>
+			                            </td>
+
+			                            <!-- Small modal -->
+					                  	<div class="modal fade modalaLihat1" tabindex="-1" role="dialog" aria-hidden="true" style="color: #000">
+					                    	<div class="modal-dialog modal-sm">
+					                      		<div class="modal-content">
+
+					                        		<div class="modal-header">
+					                          			<h4 class="modal-title" id="myModalLabel2">Hapus Artikel</h4>
+					                          			<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
+					                        		</div>
+					                        		<div class="modal-body">
+					                          			<h4>Apa anda yakin ingin menghapus konten ini ?</h4>
+					                        		</div>
+					                        		<div class="modal-footer">
+					                          			<button type="button" class="btn btn-secondary" data-dismiss="modal">Tidak</button>
+					                          			<button type="button" class="btn btn-danger">Hapus</button>
+					                        		</div>
+
+					                      		</div>
+				                    		</div>
+					                  	</div>
+				                  		<!-- /modals -->
+	                      			</tr>
+	                      			<tr class="even pointer">
+			                            <td class=" "><a href="http://waktu.my.id/short/dkfdf8">dkfdf8</a></td>
+			                            <td class=" ">Kisah Hidup Nabi Ayun AS</td>
+			                            <td class=" ">Kisah</td>
+			                            <td class=" ">Kisah Nabi</td>
+			                            <td class=" ">
+			                            	<span class="badge badge-warning">Menunggu Moderasi</span>
+			                            	<span class="badge badge-danger">Ditolak</span>
+			                            	<span class="badge badge-success">Tayang</span>
+			                            </td>
+			                            <td class=" ">
+			                            	<ul class="nav nav-pills" role="tablist">
+                    							<li role="presentation" class="dropdown">
+                      								<a href="#" class="dropdown-toggle" data-toggle="dropdown" style="padding: 0px">Action<span class="caret"></span></a>
+                              						<div class="dropdown-menu">
+                                  						<a class="dropdown-item" href="#">Edit</a>
+                                  						<a class="dropdown-item" href="#">Hapus</a>
+                                  						<a class="dropdown-item" href="#">Lihat</a>
+                                  					</div>
+                    							</li>
+                  							</ul>
+			                            </td>
+	                      			</tr>
+	                    		</tbody>
+	                  		</table>
+	                	</div>
+                	</div>
+              	</div>
+          </div>
+        </div>
 	</div>
 </div>
--- application/views/template/cms/cms_pebri_template.php
+++ application/views/template/cms/cms_pebri_template.php
@@ -20,6 +20,13 @@
 	    <!-- jQuery custom content scroller -->
 	    <link href="<?php echo base_url('vendors/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.min.css') ?>" rel="stylesheet"/>
 
+	    <!-- TABELE -->
+	    <link href="<?php echo base_url('vendors/datatables.net-bs/css/dataTables.bootstrap.min.css') ?>" rel="stylesheet">
+	    <link href="<?php echo base_url('vendors/datatables.net-buttons-bs/css/buttons.bootstrap.min.css') ?>" rel="stylesheet">
+	    <link href="<?php echo base_url('vendors/datatables.net-fixedheader-bs/css/fixedHeader.bootstrap.min.css') ?>" rel="stylesheet">
+	    <link href="<?php echo base_url('vendors/datatables.net-responsive-bs/css/responsive.bootstrap.min.css') ?>" rel="stylesheet">
+	    <link href="<?php echo base_url('vendors/datatables.net-scroller-bs/css/scroller.bootstrap.min.css') ?>" rel="stylesheet">
+
 	    <!-- Custom Theme Style -->
 	    <link href="<?php echo base_url('assets/css/custom.min.css') ?>" rel="stylesheet">
 	</head>
@@ -73,6 +80,23 @@
 	    <script src="<?php echo base_url('vendors/malihu-custom-scrollbar-plugin/jquery.mCustomScrollbar.concat.min.js') ?>"></script>
 
 	    <!-- Custom Theme Scripts -->
-	    <script src="<?php echo base_url('assets/js/custom.js') ?>"></script> -->
+	    <script src="<?php echo base_url('assets/js/custom.js') ?>"></script>
+
+	    <!-- Datatables -->
+	    <script src="<?php echo base_url('vendors/datatables.net/js/jquery.dataTables.min.js') ?>"></script>
+	    <script src="<?php echo base_url('vendors/datatables.net-bs/js/dataTables.bootstrap.min.js') ?>"></script>
+	    <script src="<?php echo base_url('vendors/datatables.net-buttons/js/dataTables.buttons.min.js') ?>"></script>
+	    <script src="<?php echo base_url('vendors/datatables.net-buttons-bs/js/buttons.bootstrap.min.js') ?>"></script>
+	    <script src="<?php echo base_url('vendors/datatables.net-buttons/js/buttons.flash.min.js') ?>"></script>
+	    <script src="<?php echo base_url('vendors/datatables.net-buttons/js/buttons.html5.min.js') ?>"></script>
+	    <script src="<?php echo base_url('vendors/datatables.net-buttons/js/buttons.print.min.js') ?>"></script>
+	    <script src="<?php echo base_url('vendors/datatables.net-fixedheader/js/dataTables.fixedHeader.min.js') ?>"></script>
+	    <script src="<?php echo base_url('vendors/datatables.net-keytable/js/dataTables.keyTable.min.js') ?>"></script>
+	    <script src="<?php echo base_url('vendors/datatables.net-responsive/js/dataTables.responsive.min.js') ?>"></script>
+	    <script src="<?php echo base_url('vendors/datatables.net-responsive-bs/js/responsive.bootstrap.js') ?>"></script>
+	    <script src="<?php echo base_url('vendors/datatables.net-scroller/js/dataTables.scroller.min.js') ?>"></script>
+	    <script src="<?php echo base_url('vendors/jszip/dist/jszip.min.js') ?>"></script>
+	    <script src="<?php echo base_url('vendors/pdfmake/build/pdfmake.min.js') ?>"></script>
+	    <script src="<?php echo base_url('vendors/pdfmake/build/vfs_fonts.js') ?>"></script>
 	</body>
 </html>
