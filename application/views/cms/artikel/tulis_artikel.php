2b274e766b9340f3c98e0ec504597f5540e91a89
Necropay <enja444@gmail.com>
2019-12-05 13:16:08 +0700

    Update form tulis artikel, tambah sub kategori

--- application/views/cms/artikel/tulis_artikel.php
+++ application/views/cms/artikel/tulis_artikel.php
@@ -4,70 +4,19 @@
 		<div class="right_col" role="main">
 			<div class="">
 				<div class="clearfix"></div>
-				<!-- UPLOAD GAMBAR -->
-				<div class="panel">
-					<div class="button_outer">
-						<div class="btn_upload">
-						<input type="file" id="upload_file" name="">
-							Upload Gambar
-						</div>
-						<div class="processing_bar"></div>
-					<div class="success_box"></div>
-					</div>
-				</div>
-				<div class="error_msg"></div>
-				<div class="uploaded_file_view" id="uploaded_view">
-					<span class="file_remove">X</span>
-				</div>
-				<!-- END UPLOAD GAMBAR -->
 				<div class="row">
 				<div class="col-md-12 ">
 					<div class="x_panel">
 						<div class="x_content"><br />
 						<div class="form-group row ">
-							<label class="control-label col-md-3 col-sm-3 "><h2>Judul Artikel</h2></label>
+							<label class="control-label col-md-3 col-sm-3 "><h2>Judul :</h2></label>
 							<div class="col-md-9 col-sm-9 ">
 								<input type="text" class="form-control">
 							</div>
 						</div>
-						<div class="form-group row">
-							<label class="control-label col-md-3 col-sm-3 "><h2>Kategori</h2></label>
-							<div class="col-md-9 col-sm-9 ">
-								<select class="form-control">
-									<option>Pilih Kategori</option>
-									<option>Option one</option>
-									<option>Option two</option>
-									<option>Option three</option>
-									<option>Option four</option>
-								</select>
-							</div>
-						</div>
-						<div class="control-group row">
-							<label class="control-label col-md-3 col-sm-3 "><h2>Tags</h2></label>
-							<div class="col-md-9 col-sm-9 ">
-								<input id="tags_1" type="text" class="tags form-control" value="social, adverts, sales" />
-								<div id="suggestions-container" style="position: relative; float: left; width: 250px; margin: 10px;"></div>
-							</div>
-						</div>
-						<div class="col-md-12 col-sm-12 ">
-							<div class="x_panel">
-								<div class="x_title">
-								<h2><small>Isi</small> Artikel</h2>
-									<ul class="nav navbar-right panel_toolbox">
-										<li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-										</li>
-										<li class="dropdown">
-											<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-											<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-												<a class="dropdown-item" href="#">Settings 1</a>
-												<a class="dropdown-item" href="#">Settings 2</a>
-											</div>
-										</li>
-										<li><a class="close-link"><i class="fa fa-close"></i></a>
-										</li>
-									</ul>
-									<div class="clearfix"></div>
-								</div>
+							<div class="form-group row ">
+								<h2 style="margin-left:10px;">Isi Artikel :</h2>
+
 								<div class="x_content">
 									<div id="alerts"></div>
 									<div class="btn-toolbar editor" data-role="editor-toolbar" data-target="#editor-one">
@@ -136,14 +85,60 @@
 								<br />
 								</div>
 							</div>
+						<div class="form-group row">
+							<label class="control-label col-md-3 col-sm-3 "><h2>Kategori</h2></label>
+							<div class="col-md-9 col-sm-9 ">
+								<select class="form-control">
+									<option></option>
+									<option>Option one</option>
+									<option>Option two</option>
+									<option>Option three</option>
+									<option>Option four</option>
+								</select>
+							</div>
 						</div>
+						<div class="form-group row">
+							<label class="control-label col-md-3 col-sm-3 "><h2>Sub Kategori</h2></label>
+							<div class="col-md-9 col-sm-9 ">
+								<select class="form-control">
+									<option></option>
+									<option>Option one</option>
+									<option>Option two</option>
+									<option>Option three</option>
+									<option>Option four</option>
+								</select>
+							</div>
+						</div>
+						<div class="control-group row">
+							<label class="control-label col-md-3 col-sm-3 "><h2 style="margin-left:10px;">Tags</h2></label>
+							<div class="col-md-9 col-sm-9 ">
+								<input id="tags_1" type="text" class="tags form-control" value="social, adverts, sales" />
+								<div id="suggestions-container" style="position: relative; float: left; width: 250px; margin: 10px;"></div>
+							</div>
+						</div>
+						<!-- UPLOAD GAMBAR -->
+						<div class="panel">
+							<div class="button_outer">
+								<div class="btn_upload">
+								<input type="file" id="upload_file" name="">
+									Upload Gambar Artikel
+								</div>
+								<div class="processing_bar"></div>
+							<div class="success_box"></div>
+							</div>
+						</div>
+						<div class="error_msg"></div>
+						<div class="uploaded_file_view" id="uploaded_view">
+							<span class="file_remove">X</span>
+						</div>
+						<!-- END UPLOAD GAMBAR -->
 						<div class="ln_solid"></div>
 						<div class="form-group">
-						<div class="col-md-9 col-sm-9  offset-md-3">
-						<button type="button" class="btn btn-primary">Cancel</button>
-						<button type="reset" class="btn btn-primary">Reset</button>
-						<button type="submit" class="btn btn-success">Submit</button>
-						</div>
+							<div class="col-md-9 col-sm-9  offset-md-3">
+								<button type="button" class="btn btn-primary">Cancel</button>
+								<button type="reset" class="btn btn-primary">Reset</button>
+								<button type="submit" class="btn btn-success">Submit</button>
+							</div>
 						</div>
 						</div>
 						</div>
