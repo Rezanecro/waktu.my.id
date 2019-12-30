e2b5d2c570668458b3200d941f34d6a9d261e52e
aquarink <junkuwieazy@gmail.com>
2019-12-30 18:55:47 +0700

    Penambahan Routing
    Penambahan Routing Send Email dan Logic pada register

--- application/config/routes.php
+++ application/config/routes.php
@@ -60,6 +60,7 @@ $route['lupa']						= 'Pengguna/lupa_password';
 
 $route['panel']						= 'Cms';
 $route['panel/pengguna']			= 'Pengguna';
+$route['panel/email']			= 'Pengguna';
 $route['panel/artikel']				= 'Artikel';
 $route['panel/video']				= 'Video';
 $route['panel/jadwal']				= 'Jadwal';
@@ -123,7 +124,10 @@ $route['panel/moderasi/jadwal/disetujui']	= 'Jadwal/daftar_disetujui';
 $route['panel/moderasi/donasi/menunggu']	= 'Donasi/daftar_moderasi';
 $route['panel/moderasi/donasi/disetujui']	= 'Donasi/daftar_disetujui';
 
-
+// Email
+$route['panel/email/test']					= 'Pengguna/testEmail';
+$route['panel/email/verifikasi']			= 'Pengguna/email_verifikasi';
+$route['panel/email/validasi']				= 'Pengguna/email_validasi';
 
 // AJAX
 $route['ajax']					= 'Front';
