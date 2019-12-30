c98b5634e5530a133d068785cd0b8053efd4c4d2
aquarink <junkuwieazy@gmail.com>
2019-12-30 15:58:29 +0700

    Ajax Masuk
    penambahan ajax masuk

--- assets/js/waktu/ajax.masuk.js
+++ assets/js/waktu/ajax.masuk.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
-	
+	$("#modlasPeringatan").on('hide.bs.modal', function () {
+    	$('#kodeResponse').html('');
+		$('#pesanResponse').html('');
+    });
 });
 
 $("#masukFormId").on("click", function() {
@@ -9,10 +12,50 @@ $("#masukFormId").on("click", function() {
 	if(
 		emaiUserTxt.length !== 0 &&
 		passwordUserTxt.length !== 0) {
+		if(passwordUserTxt.length >= 6) {
+			// FORM DATA
+			var formData = new FormData();
+			formData.append('emaiUserTxt', emaiUserTxt);
+			formData.append('passwordUserTxt', passwordUserTxt);
 
-		console.log('ada')
-		
+			console.log(formData);
+
+			$.ajax({
+		    	beforeSend: function() {
+		    		console.log('Loading...!')
+		    	},
+			    type: "POST",
+			    url: baseUrl+"ajax/pengguna/masuk",
+			    data: formData,
+			    processData: false,
+	    		contentType: false,
+			    success: function(response) {
+			    	$('#modlasPeringatan').modal('show');
+
+			        $('#kodeResponse').append(response.code);
+					$('#pesanResponse').append(response.msg);
+			    },
+			    error: function(XMLHttpRequest, textStatus, errorThrown) {
+			    	$('#modlasPeringatan').modal('show');
+
+			    	$('#kodeResponse').append(textStatus);
+					$('#pesanResponse').append(errorThrown);
+			        console.log("Status: " + textStatus);
+			        console.log("Error: " + errorThrown);
+			    }
+			});
+		} else {
+			$('#modlasPeringatan').modal('show');
+
+			$('#kodeResponse').append(404);
+			$('#pesanResponse').append('Password minimal 6 karakter');
+			console.log('Password minimal 6 karakter');
+		}		
 	} else {
-		alert('Ada form yang kosong, mohon diisi!');
+		$('#modlasPeringatan').modal('show');
+
+		$('#kodeResponse').append(404);
+		$('#pesanResponse').append('Ada form yang kosong, mohon diisi');
+		console.log('Ada form yang kosong, mohon diisi');
 	}
 });
